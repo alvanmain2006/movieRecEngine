8263b26a26e65c25a0f2f04fa60842c2cd04e62f
Tri Vo <trivo006wa@gmail.com>
2026-04-02 15:58:28 -0700

    alternative way to load

--- scripts/loads_data.py
+++ scripts/loads_data.py
@@ -25,4 +25,21 @@ if __name__ == "__main__":
     movies_df = load_data(movies_file)
     ratings_df = load_data(ratings_file)
     tags_df = load_data(tags_file)
+    print(link_df.head())  # Print the first few rows of the loaded data
+
+    # dir = "data/raw/ml-latest-small/"
+    # links_file = os.path.join(dir, "links.csv")
+    # movies_file = os.path.join(dir, "movies.csv")
+    # ratings_file = os.path.join(dir, "ratings.csv")
+    # tags_file = os.path.join(dir, "tags.csv")
     
+    # files = ["links.csv", "movies.csv", "ratings.csv", "tags.csv"]
+    # df_list = [None] * len(files)
+    # for i, file in enumerate(files):
+    #     file_path = os.path.join(dir, file)
+    #     if not os.path.exists(file_path):
+    #         print(f"Warning: The file {file} does not exist in the directory {dir}. Please check the path and try again.")
+    #     else:
+    #         df_list[i] = load_data(file_path)
+
+    # print(df_list[0].head())
