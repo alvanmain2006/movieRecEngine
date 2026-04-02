bc110a3ba9e598a1e4c2b2db4c74ad78b10a5981
Tri Vo <trivo006wa@gmail.com>
2026-04-01 23:41:59 -0700

    Initial project setup with data loading script

---
+++ scripts/loads_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+def load_data(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"The file {file_path} does not exist.") # check if the file exists before loading 
+    
+    try:
+        data = pd.read_csv(file_path)
+        print(f"Data loaded successfully from {file_path}")
+        return data
+    except Exception as e:
+        print(f"An error occurred while loading the data: {e}")
+        raise e
+
+if __name__ == "__main__":
+
+    dir = "data/raw/ml-latest-small/"
+    links_file = os.path.join(dir, "links.csv")
+    movies_file = os.path.join(dir, "movies.csv")
+    ratings_file = os.path.join(dir, "ratings.csv")
+    tags_file = os.path.join(dir, "tags.csv")
+
+    link_df = load_data(links_file)
+    movies_df = load_data(movies_file)
+    ratings_df = load_data(ratings_file)
+    tags_df = load_data(tags_file)
+    
